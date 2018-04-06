4f5e7e5187c3df4b5f877becc971055314874759
Yenalgloshi <jasonbegay@msn.com>
2018-04-06 14:15:56 -0600

    react setup with errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
-import Form from './components/Form/Form';
-import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard';
+import Form from './components/Form';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       < Dashboard />
+       <Dashboard />
       </div>
     );
   }
